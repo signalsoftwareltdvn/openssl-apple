4c152e7f49de4b0ae6e4059ba1425bd969ca4a84
Davide De Rosa <keeshux@gmail.com>
2026-04-12 18:11:12 +0000

    3.6.2

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let filename = "openssl.xcframework.zip"
-let version = "3.6.201"
-let checksum = "aaf0170c1503ef108d7f383e2b05788907c2a8f4c38fb8c692981853374805ca"
+let version = "3.6.300"
+let checksum = "ecb4b3972de7967ccaa37518c502a45b79f7a82bc4e10165455ac96309e64558"
 
 let package = Package(
     name: "openssl-apple",
