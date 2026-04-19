1e1987ca61deeaacf950c525b1d308eda5fd4b72
signalsoftwareltdvn <signalsoftwareltdvn@gmail.com>
2026-04-19 17:44:57 +0700

    Update OpenSSL version and checksum in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let filename = "openssl.xcframework.zip"
-let version = "3.6.300"
-let checksum = "ecb4b3972de7967ccaa37518c502a45b79f7a82bc4e10165455ac96309e64558"
+let version = "3.6.2"
+let checksum = "sha256:e396be243c6cdf933e1b8ab24c1afffc9a1507a3420e7733f56396ebfe11e78a"
 
 let package = Package(
     name: "openssl-apple",
@@ -19,10 +19,10 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/partout-io/openssl-apple/releases/download/\(version)/\(filename)",
+            url: "https://github.com/signalsoftwareltdvn/openssl-apple/releases/download/3.6.2/openssl.xcframework.zip",
             checksum: checksum
         ),
-
+//https://github.com/signalsoftwareltdvn/openssl-apple/releases/download/3.6.2/openssl.xcframework.zip
         // local development
 //        .binaryTarget(
 //            name: "openssl",
